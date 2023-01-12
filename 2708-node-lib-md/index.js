a4a107a5c22a981943aa800b68c0946d81d60a05
Gabrielle Andrade Da Silva <105249937+Gbiiandrad@users.noreply.github.com>
2023-01-11 22:17:32 -0300

    parte 2

--- 2708-node-lib-md/index.js
+++ 2708-node-lib-md/index.js
@@ -1,12 +1,29 @@
 import fs from 'fs';
-import chalk from 'chalk';
+import chalk, { chalkStderr } from 'chalk';
 
-function pegaArquivo(caminhoDoArquivo) {
+// Caso o arquivo não for encontrado "Mensagem de erro"
+function trataErro(erro) {
+    console.log(erro);
+    throw new Error(chalk.red (erro.code, 'Não há arquivo no diretório') );  //lançar algo
 
-    const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, ( _, texto) => {
-        console.log(chalk.green(texto));
-    });
 }
 
-pegaArquivo('./arquivos/texto.md');
+// asysnc / await: 
+async function pegaArquivo(caminhoDoArquivo) {
+
+    try {
+        
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+    
+        console.log(chalk.green (texto)); // lembrando q o chalk é para colocar a cor 
+    } 
+
+    catch (erro) {
+        trataErro(erro);
+    }
+}
+
+
+pegaArquivo('./arquivos/texto.md'); // certo
+pegaArquivo('./arquivos/'); // para a mensagem de erro
